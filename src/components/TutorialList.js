303523b1180d556e50487533b0de29cd5801a3c0
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-11-12 00:13:59 +0530

    tut list title

--- src/components/TutorialList.js
+++ src/components/TutorialList.js
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TutorialDataService from "../services/TutorialService";
+import { Link } from "react-router-dom";
 
 const TutorialList = () => {
-  return <div></div>;
+  const [tutorials, setTutorials] = useState([]);
+  const [currentTutorial, setCurrentTutorial] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  useEffect(() => {
+    retrieveTutorials();
+  }, []);
+
+  const retrieveTutorials = () => {
+    TutorialDataService.getAll()
+      .then((response) => {
+        setTutorials(response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  const refreshList = () => {
+    retrieveTutorials();
+    setCurrentTutorial(null);
+    setCurrentIndex(-1);
+  };
+  // const onChangeSearchTitle = (e) => {
+  //   const searchTitle = e.target.value;
+  //   setSearchTitle(searchTitle);
+  // };
+  const setActiveTutorial = (tutorial, index) => {
+    setCurrentTutorial(tutorial);
+    setCurrentIndex(index);
+  };
+
+  // const removeAllTutorials = () => {
+  //   TutorialDataService.removeAll()
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       refreshList();
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //     });
+  // };
+
+  // const findByTitle = () => {
+  //   TutorialDataService.findByTitle(searchTitle)
+  //     .then((response) => {
+  //       setTutorials(response.data);
+  //       console.log(response.data);
+  //     })
+  //     .catch((e) => {
+  //       console.log(e);
+  //     });
+  // };
+
+  return (
+    <div>
+      <div className="col-md-6">
+        <h4>Tutorials List</h4>
+
+        <ul className="list-group">
+          {tutorials &&
+            tutorials.map((tutorial, index) => (
+              <li
+                className={
+                  "list-group-item " + (index === currentIndex ? "active" : "")
+                }
+                onClick={() => setActiveTutorial(tutorial, index)}
+                key={index}
+              >
+                {tutorial.title}
+              </li>
+            ))}
+        </ul>
+        {/* <button
+          className="m-3 btn btn-sm btn-danger"
+          onClick={removeAllTutorials}
+        >
+          Remove All
+        </button> */}
+      </div>
+    </div>
+  );
 };
 
 export default TutorialList;
