4b0dbeb4744b90ff22ed4a6782995dd11ae769e7
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-11-12 00:47:10 +0530

    dislayed details for selected list item

--- src/components/TutorialList.js
+++ src/components/TutorialList.js
@@ -59,7 +59,7 @@ const TutorialList = () => {
   // };
 
   return (
-    <div>
+    <div className="row">
       <div className="col-md-6">
         <h4>Tutorials List</h4>
 
@@ -77,12 +77,50 @@ const TutorialList = () => {
               </li>
             ))}
         </ul>
+
         <button
           className="m-3 btn btn-sm btn-danger"
           onClick={removeAllTutorials}
         >
           Remove All
         </button>
+      </div>
+      <div className="col-md-6">
+        {currentTutorial ? (
+          <div>
+            <h4>Tutorial</h4>
+            <div>
+              <label>
+                <strong>Title:</strong>
+              </label>{" "}
+              {currentTutorial.title}
+            </div>
+            <div>
+              <label>
+                <strong>Description:</strong>
+              </label>{" "}
+              {currentTutorial.description}
+            </div>
+            <div>
+              <label>
+                <strong>Status:</strong>
+              </label>{" "}
+              {currentTutorial.published ? "Published" : "Pending"}
+            </div>
+
+            <Link
+              to={"/tutorials/" + currentTutorial.id}
+              className="badge badge-warning"
+            >
+              Edit
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Tutorial...</p>
+          </div>
+        )}
       </div>
     </div>
   );
